2d081cd7771cf21a6b789bad5aae8b4387ae6b0d
Nathan Sweet <natexsweet@gmail.com>
2023-09-27 11:49:28 -0700

    printing square roots from 1 to 1000

--- CS361_Exercise0.rb
+++ CS361_Exercise0.rb
@@ -1,4 +1,9 @@
 #Nathan Sweet
 #CS 361 
 #Exercise 0
-put "hello";
+$i = 1;                 #initializing an iterator
+$targetNum = 1000;      #initializing an endcase for the iterator
+while $i<=$targetNum do #while the iterator is less than the endcase
+    puts "Square root of #$i is "+Math.sqrt($i).to_s;   #calculating the square root of the iterator and printing it
+    $i+=1               #incrementing iterator
+end                     #ending while loop
